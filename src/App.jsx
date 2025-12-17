70dd2475fa4124968b7030af88579b08c47745c1
wasif kareem <wasifkareem2762@gmail.com>
2025-12-17 15:02:34 +0530

    :web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Footer from "./comp/Footer";
+import { Analytics } from "@vercel/analytics/next"
+
 
 function App() {
   return (
